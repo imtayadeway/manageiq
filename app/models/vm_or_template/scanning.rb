7e67c9c90dff6f5bc493d24d73ed540d3f3b2cbb
Tim Wade <hello@timjwade.com>
2017-01-13 13:04:59 -0800

    asf

--- app/models/vm_or_template/scanning.rb
+++ app/models/vm_or_template/scanning.rb
@@ -11,6 +11,7 @@ module VmOrTemplate::Scanning
   def scan(userid = "system", options = {})
     # Check if there are any current scan jobs already waiting to run
     puts self.class.name
+    puts self.class.ancestors.inspect
     VmScan.methods(false).each { |m| puts [m, VmScan.method(m).source_location].inspect }
     VmScan.instance_methods(false).each { |m| puts [m, VmScan.instance_method(m).source_location].inspect }
 
