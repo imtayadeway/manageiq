158fa9d18eb2e8a4a09ec54baf85972586b615ca
Tim Wade <hello@timjwade.com>
2017-01-17 09:00:12 -0800

    asdf

--- app/models/vm_or_template/scanning.rb
+++ app/models/vm_or_template/scanning.rb
@@ -40,7 +40,8 @@ module VmOrTemplate::Scanning
     begin
       self.last_scan_attempt_on = Time.now.utc
       save
-      job = ::Job.create_job("VmScan", options)
+      puttfs "Scanning/Job oid: #{Job.object_id}"
+      job = Job.create_job("VmScan", options)
       return job
     rescue => err
       _log.log_backtrace(err)
