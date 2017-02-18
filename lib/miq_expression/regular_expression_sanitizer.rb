63a7eea4f0f83dcd9478606487cea95a6facf7cf
Tim Wade <hello@timjwade.com>
2017-02-17 17:00:51 -0800

    wip

--- lib/miq_expression/regular_expression_sanitizer.rb
+++ lib/miq_expression/regular_expression_sanitizer.rb
@@ -2,9 +2,9 @@ class MiqExpression::RegularExpressionSanitizer
   def self.sanitize(string)
     string = string.dup
     string = "/#{string}/" unless string.starts_with?("/")
-    slice = if value.ends_with?("/")
+    slice = if string.ends_with?("/")
               1..-2
-            elsif value[-2] == "/"
+            elsif string[-2] == "/"
               1..-3
             end
 
